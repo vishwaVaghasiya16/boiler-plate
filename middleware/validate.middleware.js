6710c088ee6ecfae79787619e634bee874345c13
vishwaVaghasiya16 <vishwa.logicgoinfotech@gmail.com>
2025-04-17 16:39:08 +0530

    modify validate function

--- middleware/validate.middleware.js
+++ middleware/validate.middleware.js
@@ -15,6 +15,25 @@ const pick = (object, keys) => {
 };
 
 /**
+ * Get first required key from Joi schema
+ */
+const getFirstRequiredFieldName = (schemaObj) => {
+  if (!schemaObj || typeof schemaObj.describe !== "function") return null;
+  const desc = schemaObj.describe();
+
+  if (desc.type === "object" && desc.keys) {
+    for (const key in desc.keys) {
+      const field = desc.keys[key];
+      if (field?.flags?.presence === "required") {
+        return key; // Return the first required field
+      }
+    }
+  }
+
+  return null;
+};
+
+/**
  * Middleware to validate request data, including `multipart/form-data`
  */
 const validate = (schema) => (req, res, next) => {
@@ -26,6 +45,33 @@ const validate = (schema) => (req, res, next) => {
   if (!req.files) req.files = {};
 
   const object = pick(req, Object.keys(validSchema));
+
+  const isEmptyInput = ["params", "query", "body", "files"].every((key) => {
+    return (
+      !object[key] ||
+      (typeof object[key] === "object" && Object.keys(object[key]).length === 0)
+    );
+  });
+
+  if (isEmptyInput) {
+    // Check first required key in priority order
+    const firstRequiredField =
+      getFirstRequiredFieldName(schema.body) ||
+      getFirstRequiredFieldName(schema.params) ||
+      getFirstRequiredFieldName(schema.query) ||
+      getFirstRequiredFieldName(schema.files);
+
+    const message = firstRequiredField
+      ? `"${firstRequiredField}" is required.`
+      : "Please fill in at least one of the required fields: params, query, body, or files.";
+
+    return validateResponse({
+      res,
+      error: { message },
+      statusCode: StatusCodes.BAD_REQUEST,
+    });
+  }
+
   const { value, error } = Joi.compile(validSchema)
     .prefs({ errors: { label: "key" }, abortEarly: false })
     .validate(object);
